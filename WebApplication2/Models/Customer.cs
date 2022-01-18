6805a61cffd7c29b1f5abcf2a3e290ac0e2c8aaf
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-18 11:57:20 +0530

    Add project files.

---
+++ WebApplication2/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
